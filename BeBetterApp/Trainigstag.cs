2fbe76d0919e669946f0b41d90fca9993837614c
Zenbook <Zenbook@ASUS>
2025-05-16 11:45:29 +0200

    kallse

---
+++ BeBetterApp/Trainigstag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeBetterApp
+{
+    public class Trainigstag
+    {
+        public string Tag;
+        Trainingseinheit Trainingseinheit;
+
+        Trainigstag(string tag, Trainingseinheit trainingseinheit)
+        {
+            this.Tag = tag;
+            this.Trainingseinheit = trainingseinheit;
+        }
+    }
+}
