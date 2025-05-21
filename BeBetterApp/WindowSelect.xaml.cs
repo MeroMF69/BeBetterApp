59ca149e8133520d8851e2689efe4502f7975302
Zenbook <Zenbook@ASUS>
2025-05-21 15:25:53 +0200

    adadad

--- BeBetterApp/WindowSelect.xaml.cs
+++ BeBetterApp/WindowSelect.xaml.cs
@@ -78,10 +78,10 @@ namespace BeBetterApp
                     if (!string.IsNullOrWhiteSpace(reply))
                     {
                         // Speicherort: Desktop
-                        // string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Fitnessplan.csv");
+                        //string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Fitnessplan.csv");
 
-                        File.WriteAllText(path, reply);
-                        Textblock_Text.Text = $"Fitnessplan gespeichert auf dem Desktop:\n{path}";
+                        //File.WriteAllText(path, reply);
+                        //Textblock_Text.Text = $"Fitnessplan gespeichert auf dem Desktop:\n{path}";
                     }
                     else
                     {
