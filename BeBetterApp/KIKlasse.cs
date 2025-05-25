e10529eb9aecc4f5275a9e7a2720d692113bdc65
MeroMF69 <muhammet.altuntas@student.htl-rankweil.at>
2025-05-25 14:20:43 +0200

    Die antworten von chatgpt werden abgespeichert und können dann aufgerufen werden

--- BeBetterApp/KIKlasse.cs
+++ BeBetterApp/KIKlasse.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 using OpenAI;
 using OpenAI.Chat;
 
@@ -16,11 +17,27 @@ namespace BeBetterApp
     {
         public void Ki(TextBox ausgabe)
         {
-            ChatClient client = new(model: "gpt-4o", apiKey: "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");
+            
+            ausgabe.Text = "Ihr Trainigsplan wird erstellt bitte warten!"; // Dass der Nutzer weiß dass es geladen wird
 
-            ChatCompletion completion = client.CompleteChat("Gib mir ein Trainungsplan für eine woche. Gib nur den Plan nichts dazu schreiben oder so ** hinzufügen ein komplett normaler Text. Danke!");
+            ChatClient client = new(model: "gpt-4o", apiKey: "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"); // Hier ist mein API Key und welche version von Chat gpt es benutzen soll
 
-            ausgabe.Text = completion.Content[0].Text;
+            ChatCompletion completion = client.CompleteChat("Gib mir ein Trainungsplan für eine woche. Gib nur den Plan nichts dazu schreiben oder so ** hinzufügen ein komplett normaler Text. Danke!"); // Hier wird Chatgpt eine Frage gestellt
+
+            string save = completion.Content[0].Text; // Hier wird die antwort abgespeichert
+
+            using (StreamWriter sw = new StreamWriter("Trainingsplan.js"))
+            {
+                sw.WriteLine(save); //Hier wird die Datei abgespeichert dass man auch den gelichen Trainungsplan benutzt
+
+            }
+
+            int zeilenAnzahl = File.ReadLines("Trainingsplan.js").Count(); // Anzahl an zeilen
+
+            string inhalt = File.ReadAllText("Trainingsplan.js"); // File wird gelesen
+            ausgabe.Text = inhalt; // File wird ausgegeben
+
+
         }
 
     }
