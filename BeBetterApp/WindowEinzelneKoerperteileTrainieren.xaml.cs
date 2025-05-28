04eb242aa64524aad258d988646c1967e6f88791
Dell <Dell@DESKTOP-4MOTUUD>
2025-05-28 21:28:17 +0200

    New Update

--- BeBetterApp/WindowEinzelneKoerperteileTrainieren.xaml.cs
+++ BeBetterApp/WindowEinzelneKoerperteileTrainieren.xaml.cs
@@ -23,5 +23,10 @@ namespace BeBetterApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Es geht!!!");
+        }
     }
 }
