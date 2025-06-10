344f7f96010a71b7f20d4bd9b2467203ba022bf9
Dell <Dell@DESKTOP-4MOTUUD>
2025-06-10 10:21:14 +0200

    Update

---
+++ BeBetterApp/GlobalSchedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeBetterApp
+{
+    public static class GlobalSchedule
+    {
+        public static Schedule SharedSchedule { get; } = new Schedule();
+    }
+}
