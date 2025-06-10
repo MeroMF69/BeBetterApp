5e9f92871276e0af4e1366d58a8b511d3279241a
Dell <Dell@DESKTOP-4MOTUUD>
2025-06-10 10:56:00 +0200

    Update

--- BeBetterApp/AppointmentItem.cs
+++ BeBetterApp/AppointmentItem.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace BeBetterApp
 {
-    public class AppointmentItem
+    internal class AppointmentItem
     {
-        public DateTime Date { get; set; }    
-        public string Text { get; set; }
     }
 }
